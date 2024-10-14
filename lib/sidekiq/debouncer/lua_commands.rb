a0c1de00d56e79385385070bd5fd44bc9e2c4fce
Karol Bąk <kukicola@gmail.com>
2024-10-14 17:17:20 +0200

    V3: Drop support for sidekiq 6.x and ruby 2.7

--- lib/sidekiq/debouncer/lua_commands.rb
+++ lib/sidekiq/debouncer/lua_commands.rb
@@ -5,15 +5,13 @@ require "digest/sha1"
 module Sidekiq
   module Debouncer
     module LuaCommands
-      REDIS_ERROR_CLASS = defined?(RedisClient::CommandError) ? RedisClient::CommandError : Redis::CommandError
-
       def define_lua_command(command, script)
         sha = Digest::SHA1.hexdigest(script)
         define_method(command) do |conn, keys, argv|
           retryable = true
           begin
             conn.call("EVALSHA", sha, keys.size, *keys, *argv)
-          rescue REDIS_ERROR_CLASS => e
+          rescue RedisClient::CommandError => e
             raise if !e.message.start_with?("NOSCRIPT") || !retryable
 
             # upload script to redis cache and retry
--- spec/support/context.rb
+++ spec/support/context.rb
@@ -3,29 +3,15 @@
 shared_context "sidekiq" do
   let(:time_start) { Time.new(2016, 1, 1, 12, 0, 0, 0) }
   let(:sidekiq_config) do
-    sidekiq_version = Gem::Version.new(Sidekiq::VERSION)
-    if sidekiq_version >= Gem::Version.new("7.0")
-      Sidekiq.default_configuration.tap do |config|
-        config.queues = ["default", "sample_queue"]
-      end
-    else
-      Sidekiq.queues = ["default", "sample_queue"]
-      Sidekiq[:fetch] = Sidekiq::BasicFetch.new(Sidekiq)
-      Sidekiq
+    Sidekiq.default_configuration.tap do |config|
+      config.queues = ["default", "sample_queue"]
     end
   end
   let(:queue) { Sidekiq::Queue.new("default") }
   let(:sample_queue) { Sidekiq::Queue.new("sample_queue") }
   let(:puller) { ::Sidekiq::Debouncer::Poller.new(sidekiq_config) }
   let(:schedule_set) { Sidekiq::Debouncer::Set.new }
-  let(:processor) do
-    sidekiq_version = Gem::Version.new(Sidekiq::VERSION)
-    if sidekiq_version >= Gem::Version.new("7.0")
-      ::Sidekiq::Processor.new(sidekiq_config.default_capsule) { |*args| }
-    else
-      ::Sidekiq::Processor.new(sidekiq_config) { |*args| }
-    end
-  end
+  let(:processor) { ::Sidekiq::Processor.new(sidekiq_config.default_capsule) { |*args| } }
 
   before do
     Timecop.freeze(time_start)
